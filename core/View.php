30ca7fee71fc6df0cf0e693a4af2e38af380c910
Marcos-SCO <marcos_sco@outlook.com>
2020-05-10 01:37:55 -0300

    View

--- core/View.php
+++ core/View.php
@@ -51,6 +51,8 @@ class View
 
         // Get global path
         $twig->addGlobal('BASE', Config::URL_BASE);
+        $twig->addGlobal('SESSION_ID', $_SESSION['user_id'] ?? false);
+        $twig->addGlobal('SESSION_USER_NAME', $_SESSION['user_name'] ?? false);
 
         echo $twig->render($template, $args);
     }
