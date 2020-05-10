9d49c0d0ecc17e56b28629970995c72ad207eba3
Marcos-SCO <marcos_sco@outlook.com>
2020-05-10 01:35:44 -0300

    Users

--- app/contollers/Users.php
+++ app/contollers/Users.php
@@ -18,48 +18,45 @@ class Users extends Controller
 
     public function index()
     {
-        if (isLoggedIn()) {
-            $users = $this->model->getAll();
-            // dump($users);
-
-            View::renderTemplate('user/index.html', [
-                'title' => 'Users',
-                'users' => $users,
-                'logout' => true
-            ]);
-        } else {
-            View::renderTemplate('home/index.html', [
-                'title' => 'Home'
-            ]);
+        if (!isLoggedIn()) {
+            View::renderTemplate('users/login.html');
+            return exit();
         }
+        $users = $this->model->getAll();
+        // dump($users);
+
+        View::renderTemplate('users/index.html', [
+            'title' => 'Users',
+            'users' => $users
+        ]);
     }
 
     public function create()
     {
-        View::renderTemplate('user/create.html', [
+        if (!isLoggedIn()) {
+            View::renderTemplate('users/login.html');
+            return exit();
+        }
+        View::renderTemplate('users/create.html', [
             'title' => 'Cadastro'
         ]);
     }
 
     public function store()
     {
+        if (!isLoggedIn()) {
+            View::renderTemplate('users/login.html');
+            return exit();
+        }
         // Check for post
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // Process Form
-            $data = $this->model->getPostData();
 
             // Errors
             $errors = false;
 
-            // Validate Email
-            if (empty($data['email'])) {
-                $data['email_error'] = "Digite o E-mail";
-                $errors = true;
-            }
-            if (!filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
-                $data['email_error'] = "E-mail inválido";
-                $errors = true;
-            }
+            // Process Form
+            $data = $this->model->getPostData();
+
             $this->model->customQuery("SELECT `email` FROM users WHERE email = :email", ['email' => $data['email']]);
 
             if ($this->model->rowCount() > 0) {
@@ -67,49 +64,26 @@ class Users extends Controller
                 $errors = true;
             }
 
-            // Name
-            if (empty($data['name'])) {
-                $data['name_error'] = "Digite o nome";
-                $errors = true;
-            }
-
-            // Password
-            if (empty($data['password'])) {
-                $data['password_error'] = "Digite a senha";
-                $errors = true;
-            } elseif (strlen($data['password']) < 6) {
-                $data['password_error'] = "Senha precisa no minimo de ser maior que 6 caracteres";
-                $errors = true;
-            }
-
-            // Password validate
-            if (empty($data['confirm_password'])) {
-                $data['confirm_password_error'] = "Confirme a senha";
-                $errors = true;
-            } elseif ($data['password'] != $data['confirm_password']) {
-                $data['confirm_password_error'] = "Senhas estão diferentes";
-                $errors = true;
-            }
-
             // Make sure error are empty
-            if ($errors != true) {
+            if ($errors != true && $data['errors'] != true) {
                 // Validated
 
                 // Hash password
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
 
                 // Register user
-                if ($this->model->insert(
+                if ($this->model->insertQuery(
                     'users',
                     [
                         'name' => $data['name'],
+                        'last_name' => $data['last_name'],
                         'email' => $data['email'],
                         'password' => $data['password'], 'created_at' => date("Y-m-d H:i:s")
                     ]
                 )) {
                     $flash = flash('register_success', 'Registrado com sucesso, faça Login');
 
-                    View::renderTemplate('user/login.html', [
+                    View::renderTemplate('users/login.html', [
                         'title' => 'Login',
                         'data' => $data,
                         'flash' => $flash
@@ -118,7 +92,7 @@ class Users extends Controller
                     die('Algo deu errado...');
                 }
             } else {
-                View::renderTemplate('user/create.html', [
+                View::renderTemplate('users/create.html', [
                     'title' => 'Login',
                     'data' => $data
                 ]);
@@ -133,12 +107,94 @@ class Users extends Controller
 
     public function edit($id)
     {
-        //
+        if (!isLoggedIn()) {
+            View::renderTemplate('users/login.html');
+            return exit();
+        }
+
+        $data = $this->model->customQuery("SELECT * FROM users WHERE id = :id", ['id' => $id]);
+
+        if ($data) {
+            View::renderTemplate('users/edit.html', [
+                'data' => $data
+            ]);
+        } else {
+            dump('Usuário não existe');
+        }
     }
 
-    public function update($table, $data, $id)
+    public function update()
     {
-        //
+        if (!isLoggedIn()) {
+            View::renderTemplate('users/login.html');
+            return exit();
+        }
+
+        // Check for post
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            // Errors
+            $errors = false;
+
+            // Process Form
+            // $data = $this->model->getPostData();
+
+            $name = isset($_POST['name']) ? trim($_POST['name']) : '';
+            $lastname = isset($_POST['last_name']) ? trim($_POST['last_name']) : '';
+
+            $data = [
+                'id' => $_POST['id'],
+                'name_error' => '',
+                'name' => $name,
+                'last_name_error' => '',
+                'last_name' => $lastname,
+                'last_name_error' => '',
+                'errors' => false
+            ];
+
+            // Name
+            if (empty($name)) {
+                $data['name_error'] = "Digite o nome";
+                $data['errors'] = true;
+            }
+
+            if (empty($lastname)) {
+                $data['last_name_error'] = "Digite o sobrenome";
+                $data['errors'] = true;
+            }
+
+            /*[
+                'name' => $data['name'],
+                'last_name' => $data['last_name'],
+                'email' => $data['email'],
+                'password' => $data['password'], 'created_at' => date("Y-m-d H:i:s")
+            ]*/
+
+            // Make sure error are empty
+            if ($errors != true && $data['errors'] != true) {
+                $update = $this->model->updateQuery('users', ['name' => $data['name'],
+                'last_name' => $data['last_name'],
+                'updated_at' => date("Y-m-d H:i:s")], ['id', $data['id']]);
+
+                // Update user
+                if ($this->model->rowCount()) {
+                    $flash = flash('register_success', 'Atualizado com sucesso!');
+
+                    View::renderTemplate('users/edit.html', [
+                        'title' => 'Edit',
+                        'data' => $data,
+                        'flash' => $flash
+                    ]);
+                } else {
+                    die('Algo deu errado...');
+                }
+            } else {
+                View::renderTemplate('users/edit.html', [
+                    'title' => 'Edit',
+                    'data' => $data
+                ]);
+            }
+        }
     }
 
     public function login()
@@ -171,7 +227,7 @@ class Users extends Controller
 
             $this->model->customQuery("SELECT `email` FROM users WHERE email = :email", ['email' => $data['email']]);
 
-            // Check for user/email
+            // Check for users/email
             if ($this->model->rowCount() < 0) {
                 // User not Found
                 $data['email_error'] = "Nenhum úsuário encontrado";
@@ -189,26 +245,20 @@ class Users extends Controller
                 } else {
                     $data['password_error'] = "Email ou Senha incorretos";
 
-                    View::renderTemplate('user/login.html', [
+                    View::renderTemplate('users/login.html', [
                         'title' => 'Login',
                         'data' => $data
                     ]);
                 }
             } else {
                 // Load view with errors
-                View::renderTemplate('user/login.html', [
+                View::renderTemplate('users/login.html', [
                     'title' => 'Login',
                     'data' => $data
                 ]);
             }
         } else {
-            // Load Form
-            $data = $this->model->getPostData();
-            // Load view
-            View::renderTemplate('user/login.html', [
-                'title' => 'Login',
-                'data' => $data
-            ]);
+            View::renderTemplate('users/login.html');
         }
     }
 
@@ -219,8 +269,7 @@ class Users extends Controller
         unset($_SESSION['user_name']);
 
         session_destroy();
-        View::renderTemplate('user/login.html', [
-            'title' => 'Login'
-        ]);
+
+        redirect('users/login');
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -10,7 +10,7 @@ class User extends Model
 {
     public function getAll()
     {
-        $result = $this->select('users');
+        $result = $this->selectQuery('users');
         return $result;
     }
 
@@ -32,25 +32,70 @@ class User extends Model
         // Sanitize data
         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $name = isset($_POST['name']) ? trim($_POST['name']) : '';
+        $lastname = isset($_POST['last_name']) ? trim($_POST['last_name']) : '';
         $email = isset($_POST['email']) ? trim($_POST['email']) : '';
         $password = isset($_POST['password']) ? trim($_POST['password']) : '';
         $confirmPassword = isset($_POST['confirm_password']) ? trim($_POST['confirm_password']) : '';
         $nameError = isset($_POST['name_error']) ? trim($_POST['name_error']) : '';
+        $lastnameError = isset($_POST['last_name_error']) ? trim($_POST['last_name_error']) : '';
         $emailError = isset($_POST['email_error']) ? trim($_POST['email_error']) : '';
         $passwordError = isset($_POST['password_error']) ? trim($_POST['password_error']) : '';
         $confirmPasswordError = isset($_POST['confirm_password_error']) ? trim($_POST['confirm_password_error']) : '';
 
+        $errors = false;
+
         // Add data to array
         $data = [
             'name' => $name,
+            'last_name' => $lastname,
             'email' => $email,
             'password' => $password,
             'confirm_password' => $confirmPassword,
             'name_error' => $nameError,
+            'last_name_error' => $lastnameError,
             'email_error' => $emailError,
             'password_error' => $passwordError,
-            'confirm_password_error' => $confirmPasswordError
+            'confirm_password_error' => $confirmPasswordError,
+            'errors' => $errors
         ];
+
+        // Validate Email
+        if (empty($data['email'])) {
+            $data['email_error'] = "Digite o E-mail";
+            $data['errors'] = true;
+        }
+        if (!filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
+            $data['email_error'] = "E-mail inválido";
+            $data['errors'] = true;
+        }
+        // Name
+        if (empty($data['name'])) {
+            $data['name_error'] = "Digite o nome";
+            $data['errors'] = true;
+        }
+
+        if (empty($data['last_name'])) {
+            $data['last_name_error'] = "Digite o sobrenome";
+            $data['errors'] = true;
+        }
+
+        // Password
+        if (empty($data['password'])) {
+            $data['password_error'] = "Digite a senha";
+            $data['errors'] = true;
+        } elseif (strlen($data['password']) < 6) {
+            $data['password_error'] = "Senha precisa no minimo de ser maior que 6 caracteres";
+            $data['errors'] = true;
+        }
+
+        // Password validate
+        if (empty($data['confirm_password'])) {
+            $data['confirm_password_error'] = "Confirme a senha";
+            $data['errors'] = true;
+        } elseif ($data['password'] != $data['confirm_password']) {
+            $data['confirm_password_error'] = "Senhas estão diferentes";
+            $data['errors'] = true;
+        }
 
         return $data;
     }
