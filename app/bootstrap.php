f3b445ef71c7946ead26295c2f53cfa3bf900a01
Marcos-SCO <marcos_sco@outlook.com>
2020-05-20 17:09:06 -0300

    Base global variable

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Config\Config;
 use Core\Router;
 
 require_once '../vendor/autoload.php';
 require_once '../helpers/functions.php';
 require_once '../helpers/sessionHelper.php';
+
+$BASE = Config::URL_BASE;
 
 // Router
 $router = new Router();
