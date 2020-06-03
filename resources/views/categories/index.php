44b9cd26e1e3e0e3eacb7605fc9372382caf84ce
Marcos-SCO <marcos_sco@outlook.com>
2020-06-03 14:20:23 -0300

    Styling

--- resources/views/categories/index.php
+++ resources/views/categories/index.php
@@ -1,12 +1,12 @@
+<header class="categoryHeader productHeader imgBackgroundArea">
+    <h1>Categorias</h1>
+</header>
 <article class="products flex-wrap flex-column">
-    <header class="text-center">
-        <h1>Categorias</h1>
-    </header>
     <section class="products flex-wrap card-group">
         <?php foreach ($categories as $data) { ?>
             <a href='<?= $BASE ?>/categories/show/<?= $data->id ?>'>
                 <figure class="card" style="color:#333!important">
-                    <img src="<?= $BASE ?>/<?=imgOrDefault('categories',$data->img,$data->id)?>" alt="<?= $data->img ?>" title="<?= $data->category_name ?>">
+                    <img src="<?= $BASE ?>/<?= imgOrDefault('categories', $data->img, $data->id) ?>" alt="<?= $data->img ?>" title="<?= $data->category_name ?>">
                     <figcaption class="card-body" style="height:142px;">
                         <h5 class="card-title"><?= $data->category_name ?></h5>
                         <?= "<p class='card-text'>$data->category_description</p>" ?>
