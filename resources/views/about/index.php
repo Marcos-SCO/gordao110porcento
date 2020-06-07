4858fae649ecc88f99fa28fc7d450a689ff535eb
Marcos-SCO <marcos_sco@outlook.com>
2020-06-06 22:38:01 -0300

    Styling

--- resources/views/about/index.php
+++ resources/views/about/index.php
@@ -24,20 +24,20 @@
                 <p>Somos uma lanchonete e restaurante com mais de <span id="activeYears" class="link">vinte e três</span> anos de tradição. Já servimos todo tipo de refeição com nosso extenso <span class="link" id="menu">menu</span>, peça já!. Te convidamos a se deliciar com nossas ofertas. Você terá a satisfação a 110% e garantia de qualidade. É gordão ou nada!
             </figcaption>
         </figure>
-        <figure class="">
+        <figure>
             <img src="<?= $BASE ?>/public/img/template/clients_500.png" alt="clients_500.png" title="Clientes" data-anima="right">
             <figcaption>
                 <p><strong class="lightText">MISSÃO</strong><br>Desenvolver e comercializar lanches saudaveis, procurando sempre a inovação, ou seja criando variedades em cardapíos, agregando confiança e transparência aos nossos clientes.</p>
             </figcaption>
         </figure>
-        <figure class="">
+        <figure>
             <img src="<?= $BASE ?>/public/img/template/gordao_letreiros_500.png" alt="gordao_letreiros_500.png" title="Gordão Letreiros" data-anima="right">
             <figcaption>
                 <p><strong class="lightText">VISÃO</strong><br>Ser a melhor lanchonete no seguimento, visando sempre o bem estar de nassos clientes. Comprometendo-se com a satisfação e valorizando nossas pessoas e visitantes.
                 </p>
             </figcaption>
         </figure>
-        <figure class="">
+        <figure>
             <img src="<?= $BASE ?>/public/img/template/restaurante_500.png" alt="restaurante_500.png" title="Clientes" data-anima="right">
             <figcaption>
                 <p><strong class="lightText">VALORES</strong><br>Trabalhando com responsabilidade, maturalidade emocional, educação cortesia e respeito, pontualidade e assuidade, com facilidade de comunicação, com entusiasmo, com segurança no que fazemos, vontade e disposição, aqui esses valores serão sempre prioridades, tornando-se um diferencial.
