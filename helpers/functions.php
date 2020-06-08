7223783d3cce482fba61d2b545fe11c643cd744a
Marcos-SCO <marcos_sco@outlook.com>
2020-06-08 17:55:17 -0300

    Detect Ie

--- helpers/functions.php
+++ helpers/functions.php
@@ -39,6 +39,15 @@ function imgOrDefault($table, $img, $id, $tableOp = '') {
     return $path;
 }
 
+// Detect if ua is IE
+function detectIE() {
+    $ua= htmlentities($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8' ); 
+    if (preg_match('~MSIE|Internet Explorer~i', $ua) || (strpos($ua, 'Trident/7.0' ) !== false && strpos($ua, 'rv:11.0' ) !== false)) { 
+        return true;
+    }
+    return false;
+}
+
 // Simple page redirect
 function redirect($page)
 {
