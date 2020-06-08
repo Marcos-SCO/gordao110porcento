799cc31755d6ab46a5d659abff80e1a7b6642dc4
Marcos-SCO <marcos_sco@outlook.com>
2020-06-08 00:37:07 -0300

    Owl functions

---
+++ public/js/owlFunctions.js
@@ -0,0 +1,27 @@
+// owl carousel
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        // breakpoint from 0 up
+        0: {
+            items: 1,
+        },
+        // breakpoint from 480 up
+        480: {
+            items: 2,
+        },
+        // breakpoint from 768 up
+        768: {
+            items: 4,
+        },
+        // breakpoint from 1000 up
+        1161: {
+            items: 5,
+        }
+    }
+});
