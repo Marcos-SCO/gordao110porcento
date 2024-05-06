2b75254f61f6ba9b2c72d33aa2330f5507b1d1dd
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-05-06 20:12:36 -0300

    refactor: herderTop

--- public/resources/js/headerTop.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('scroll', () => {
-
-  (window.pageYOffset >= 300)
-    ? window.addEventListener('scroll', document.getElementById('NavHeader').classList.add('fixed-top'))
-    
-    : window.addEventListener('scroll', document.getElementById('NavHeader').classList.remove('fixed-top'));
-});
