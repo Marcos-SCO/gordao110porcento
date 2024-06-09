eed152b5d5ed44f18c7f66b5cfa40ec807303c0c
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-06-08 21:31:44 -0300

    refactor: header changes

--- public/resources/views/base/header.php
+++ public/resources/views/base/header.php
@@ -16,6 +16,14 @@ $siteName = 'Gordão a 110%';
 $siteTitle = (!$isHomeController) && isset($title)
     ? $title . ' - ' . $siteName : $siteName;
 
+$getQuery = getQueryString(); // get url 
+
+function activePage($getQuery, $op)
+{
+    $active = ($getQuery[0] == $op) ? 'active' : '';
+    return $active;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -37,21 +45,16 @@ $siteTitle = (!$isHomeController) && isset($title)
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
-    <?php $getQuery = getQueryString(); // get url 
-    ?>
-
-    <!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> -->
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- Google fonts  -->
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps:wght@700&display=swap" rel="stylesheet"> -->
 
     <?php
+
+    // Google fonts <link href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps:wght@700&display=swap" rel="stylesheet">
+
     // tiny MCE 
     if ($isTinyMce) echo "<!-- Tiny MCE -->
     <script src='https://cdn.tiny.cloud/1/dksjdj5uue9ro7l3iyr2xu6basfnwgrqpkh8y5beu0m60kwl/tinymce/5/tinymce.min.js' referrerpolicy='origin'></script><script>tinymce.init({selector:'#tinyMCE'});</script>";
@@ -73,85 +76,122 @@ $siteTitle = (!$isHomeController) && isset($title)
     <?php // if (!$isHomeController) echo '<!-- Spinner --><div id="loader" class="center" style="display:none"></div>'; 
     ?>
 
-    <header class="<?= $isHomeController ? 'fixed-top' : '' ?> z-index bg-light" id="topNav" data-js="navHeader">
+    <header class="<?= $isHomeController ? 'fixed-top' : '' ?> z-index bg-light main-header" id="topNav" data-js="navHeader">
 
         <!-- Nav -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color:#f8f9fa;">
+
             <!-- Show this only on mobile to medium screens -->
-            <a class="navbar-brand d-lg-none" href="<?= $BASE ?>"><img src="<?= $BASE ?>/public/resources/img/template/gordao110Logo100.png" alt="Grodão a 110%" title="Gordão a 110%"></a>
+            <a class="navbar-brand d-lg-none" href="<?= $BASE ?>">
+                <img src="<?= $BASE ?>/public/resources/img/template/gordao110Logo100.png" alt="Gordão a 110%" title="Gordão a 110%">
+            </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse justify-content-between" id="navbarToggle" style="padding-right: 4.4rem;">
-                <?php
-                function activePage($getQuery, $op)
-                {
-                    $active = ($getQuery[0] == $op) ? 'active' : '';
-                    return $active;
-                }
-                ?>
 
                 <ul class="navbar-nav">
-                    <li class="nav-item <?= activePage($getQuery, '') ?>"><a class="nav-link" href="<?= $BASE ?>">Home<span class="visually-hidden">(current)</span></a></li>
-                    <li class="nav-item <?= ($getQuery[0] == 'products' || $getQuery[0] == 'categories') ? 'active' : '' ?>"><a class="nav-link" href="<?= $BASE ?>/products">Ofertas</a></li>
-                    <li class="nav-item <?= activePage($getQuery, 'posts') ?>"><a class="nav-link" href="<?= $BASE ?>/posts">Blog</a></li>
-                    <li class="nav-item <?= activePage($getQuery, 'gallery') ?>"><a class="nav-link" href="<?= $BASE ?>/gallery">Galeria</a></li>
+                    <li class="nav-item" data-active-page="home">
+                        <a class="nav-link" href="<?= $BASE ?>">
+                            Home<span class="visually-hidden">(current)</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item <?= ($getQuery[0] == 'products' || $getQuery[0] == 'categories') ? 'active' : '' ?>">
+                        <a class="nav-link" href="<?= $BASE ?>/products">Ofertas</a>
+                    </li>
+
+                    <li class="nav-item" data-active-page="posts">
+                        <a class="nav-link" href="<?= $BASE ?>/posts">Blog</a>
+                    </li>
+
+                    <li class="nav-item" data-active-page="gallery">
+                        <a class="nav-link" href="<?= $BASE ?>/gallery">Galeria</a>
+                    </li>
                 </ul>
 
                 <!-- Show this only lg screens and up -->
-                <a class="navbar-brand d-none d-lg-block" style="position:absolute;top:0;margin:0!important;" href="<?= $BASE ?>"><img src="<?= $BASE ?>/public/resources/img/template/gordao110Logo100.png" alt="Grodão a 110%" title="Gordão a 110%"></a>
+                <a class="navbar-brand d-none d-lg-block" style="position:absolute;top:0;margin:0!important;" href="<?= $BASE ?>">
+                    <img src="<?= $BASE ?>/public/resources/img/template/gordao110Logo100.png" alt="Gordão a 110%" title="Gordão a 110%">
+                </a>
 
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link <?= activePage($getQuery, 'about') ?>" href="<?= $BASE ?>/about">Sobre</a></li>
-                    <li class="nav-item dropdown <?= activePage($getQuery, 'contact') ?>">
-                        <a class="nav-link dropdown-toggle" style="background:#f8f9fa!important" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Contato</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= $BASE ?>/about" data-active-page="about">Sobre</a>
+                    </li>
+
+                    <li class="nav-item dropdown" data-active-page="contact">
+                        <a class="nav-link dropdown-toggle header-menu" style="background:#f8f9fa!important" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Contato</a>
+
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="<?= $BASE ?>/contact/message">Enviar Mensagem</a></li>
-                            <li><a class="dropdown-item" href="<?= $BASE ?>/contact/work">Trabalhe conosco</a></li>
+                            <li data-active-page="contact/message">
+                                <a class="dropdown-item" href="<?= $BASE ?>/contact/message">Enviar Mensagem</a>
+                            </li>
+
+                            <li data-active-page="contact/work">
+                                <a class="dropdown-item" href="<?= $BASE ?>/contact/work">Trabalhe conosco</a>
+                            </li>
                         </ul>
+
                     </li>
 
                     <?php if (isset($_SESSION['user_name']) && isset($_SESSION['user_id'])) : ?>
+
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" style="background:#f8f9fa!important" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Adicionar</a>
+
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="<?= $BASE ?>/users/create">Usuário</a></li>
-                                <li><a class="dropdown-item" href="<?= $BASE ?>/categories/create">Categorias</a></li>
-                                <li><a class="dropdown-item" href="<?= $BASE ?>/products/create">Produtos</a></li>
-                                <li><a class="dropdown-item" href="<?= $BASE ?>/posts/create">Postagens</a></li>
-                                <li><a class="dropdown-item" href="<?= $BASE ?>/gallery/create">Fotos</a></li>
+                                <li>
+                                    <a class="dropdown-item" href="<?= $BASE ?>/users/create">Usuário</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="<?= $BASE ?>/categories/create">Categorias</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="<?= $BASE ?>/products/create">Produtos</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="<?= $BASE ?>/posts/create">Postagens</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="<?= $BASE ?>/gallery/create">Fotos</a>
+                                </li>
                             </ul>
                         </li>
 
-                        <li class="nav-item dropdown <?= activePage($getQuery, 'users') ?>">
+                        <li class="nav-item dropdown" data-active-page="users">
                             <a class="nav-link dropdown-toggle" style="background:#f8f9fa!important" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"><?= $_SESSION['user_name'] ?? "" ?></a>
+
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                                 <li><a class="dropdown-item" href="<?= $BASE ?>/users/edit/<?= $_SESSION['user_id'] ?>">Meu perfil</a></li>
+
                                 <li><a class="dropdown-item" href="<?= $BASE ?>/users/show/<?= $_SESSION['user_id'] ?>">Página</a></li>
+
                                 <li><a class="dropdown-item" href="<?= $BASE ?>/users/">Usuários</a></li>
+
                                 <li><a class="dropdown-item" href="<?= $BASE ?>/logout">Sair</a></li>
                             </ul>
                         </li>
                     <?php endif; ?>
                 </ul>
             </div>
+
         </nav>
         <!-- end nav -->
 
     </header>
 
-    <?php
-
-    if ($isHomeController) {
+    <?php if ($isHomeController) {
 
         // Hero Slider
         include_once __DIR__ . '/../components/heroSlider.php';
     }
-
-    // Display flash messages
-    displayFlashMessage();
 
     ?>
 
@@ -162,3 +202,7 @@ $siteTitle = (!$isHomeController) && isset($title)
     <a href="https://api.whatsapp.com/send?phone=5511916459334&text=Olá+Marcos+tudo+bem?+Vim+por+meio+do+link+no+site+%22Gordão+a+110%%22+e+gostaria+de+conversar+com+você." class="float" target="_blank" id="whats"><i class="fa fa-whatsapp my-float"></i></a>
 
     <main>
+        <?php // Display flash messages
+        displayFlashMessage();
+
+        ?>
