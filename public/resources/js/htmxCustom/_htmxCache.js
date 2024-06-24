1d401767a59364723c11538899ab2b11e92bc604
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-06-24 06:50:43 -0300

    refactor: complete reload page to avoid htmx swap errors

--- public/resources/js/htmxCustom/_htmxCache.js
+++ public/resources/js/htmxCustom/_htmxCache.js
@@ -1,10 +1,43 @@
-document.addEventListener('htmx:historyCacheMiss', function (evt) {
-  // htmx.config.cache = "none";
-  // htmx.config.historyEnabled = false;
+import iCounter from "../helpers/counter";
+
+const { incrementCounter, getCounter } = iCounter();
+
+function reloadDocument(time = 500) {
+  console.clear();
+  console.warn('Reloading page after swap error...');
+
+  setTimeout(() => {
+    console.warn('Page reload...');
+
+    window.location.reload();
+  }, time)
+}
+
+function reloadBodyContent() {
+  incrementCounter();
   
-  window.htmx.ajax('GET',  window.location.href, {
+  if (getCounter() > 3) {
+    console.log(`Reload count: ${getCounter()}`);
+
+    reloadDocument(100);
+
+    return;
+  }
+
+  window.htmx.ajax('GET', window.location.href, {
     target: 'body',
     select: 'body > *',
     swap: 'outerHTML',
   });
+}
+
+document.addEventListener('htmx:historyCacheMiss', function (evt) {
+  // htmx.config.cache = "none";
+  // htmx.config.historyEnabled = false;
+
+  reloadBodyContent();
+});
+
+document.addEventListener('htmx:swapError', function (evt) {
+  reloadDocument();
 });
--- public/resources/views/base/header.php
+++ public/resources/views/base/header.php
@@ -44,7 +44,7 @@ function activePageClass(array $pagesToActivate, string $pageName)
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" hx-swap-oob="true">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
