dbfedb30ed79b3966e3e594674289c138a69ba37
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-06-24 04:20:07 -0300

    feat: debounce function

---
+++ public/resources/js/helpers/debounce.js
@@ -0,0 +1,15 @@
+function debounce(func, wait) {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+    
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
+export { debounce };
