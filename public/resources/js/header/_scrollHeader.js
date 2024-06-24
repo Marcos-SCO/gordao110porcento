d2802c23819ca4b9cf2f5c76d6e9c279bc761f76
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-06-24 05:14:36 -0300

    refactor: header scroll animations

--- public/resources/js/header/_scrollHeader.js
+++ public/resources/js/header/_scrollHeader.js
@@ -1,24 +1,38 @@
+import { debounce } from "../helpers/debounce";
+
 function navHeaderScroll() {
   const navHeader = document.querySelector('[data-js="navHeader"] [data-js="header-inner-container"]');
+  
   if (!navHeader) return;
 
   let lastScrollY = window.scrollY;
 
-  document.addEventListener('scroll', () => {
+  const scrollHandler = debounce(() => {
 
     let pageYOffsetMoreThan = window.scrollY >= 400;
 
     let currentScrollY = window.scrollY;
 
     // if (pageYOffsetMoreThan || (currentScrollY < lastScrollY)) navHeader.classList.add('fixed-top');
-    if ((currentScrollY < lastScrollY)) navHeader.classList.add('fixed-top');
+    if ((currentScrollY < lastScrollY)) {
+      navHeader.classList.add('fixed-top');
+      navHeader.classList.remove('hide');
+    }
 
     // if (!pageYOffsetMoreThan || (currentScrollY > lastScrollY)) navHeader.classList.remove('fixed-top');
-    if ((currentScrollY > lastScrollY)) navHeader.classList.remove('fixed-top');
+    if ((currentScrollY > lastScrollY)) {
+      navHeader.classList.remove('fixed-top');
+      navHeader.classList.add('hide');
+    }
 
     lastScrollY = currentScrollY;
 
-  });
+    console.log('iaaa')
+
+  }, 100);
+
+  document.addEventListener('scroll', scrollHandler, { passive: true });
+
 }
 
 document.addEventListener('DOMContentLoaded', navHeaderScroll);
