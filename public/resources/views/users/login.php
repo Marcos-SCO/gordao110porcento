498e03ad2c3614b4c9ea77181916b5c98ae10766
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-07-05 16:13:47 -0300

    refactor: user login template changes

--- public/resources/views/users/login.php
+++ public/resources/views/users/login.php
@@ -1,29 +1,38 @@
-<style><?= (detectIE() != true) ? 'main {display:flex;justify-content:center;}' : '' ?> main {height: 100%!important;background-image:url('<?=$BASE?>/public/resources/img/template/aboutBackgroundSection.png');background-repeat:no-repeat;background-size:cover;}#whats{display:none!important}</style>
+<?php
 
-<section class="d-flex flex-column justify-content-center align-items-center container">
+$formActionUrl = $BASE . '/login';
+
+?>
+
+<section class="login-section-container d-flex flex-column justify-content-center align-items-center container" data-js="login-section">
 
     <header>
         <h1>Logar no sistema</h1>
     </header>
 
-    <div class="col-md-6 mx-auto">
-        <div class="card card-body mt5">
+    <div class="col-12 col-lg-6 mx-auto">
+        <div class="form-container card card-body mt-5">
             <p>Preencha os campos</p>
-            
-            <form action="<?= $BASE ?>/login" method="post">
+
+            <form action="<?= $formActionUrl ?>" method="post" hx-post="<?= $formActionUrl ?>" hx-target="body" hx-swap="outerHTML">
+
                 <!-- Email -->
-                <div class="form-group">
-                    <label for="email">E-mail: <sup>*</sup></label>
-                    <input type="email" name="email" id="email" class="form-control form-control <?= isset($error['email_error']) && $error['email_error'] != '' ? 'is-invalid' : '' ?>" value="<?= $data['email'] ?? '' ?>">
+                <div class="form-group mb-3">
+                    <label for="email" class="form-label">E-mail: <sup>*</sup></label>
+
+                    <input type="email" name="email" id="email" class="form-control <?= isset($error['email_error']) && $error['email_error'] != '' ? 'is-invalid' : '' ?>" value="<?= $data['email'] ?? '' ?>">
+
                     <span class="invalid-feedback">
                         <?= $error['email_error'] ?? '' ?>
                     </span>
                 </div>
 
                 <!-- Password -->
-                <div class="form-group">
-                    <label for="password">Senha: <sup>*</sup></label>
-                    <input type="password" name="password" id="password" class="form-control form-control <?= isset($error['password_error']) && $error['password_error'] != '' ? 'is-invalid' : '' ?>">
+                <div class="form-group mb-4">
+                    <label for="password" class="form-label">Senha: <sup>*</sup></label>
+
+                    <input type="password" name="password" id="password" class="form-control <?= isset($error['password_error']) && $error['password_error'] != '' ? 'is-invalid' : '' ?>">
+
                     <span class="invalid-feedback">
                         <?= $error['password_error'] ?? '' ?>
                     </span>
@@ -31,7 +40,7 @@
 
                 <div class="row">
                     <div class="col">
-                        <input type="submit" value="Login" class="btn btn-success btn-block">
+                        <input type="submit" value="Login" class="btn btn-success w-100 mb-3">
                     </div>
                 </div>
 
