49324a95b82ed10fcd55df134e7053440a6dd7e3
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-07-11 02:19:05 -0300

    refactor: users show template params

--- public/resources/views/users/show.php
+++ public/resources/views/users/show.php
@@ -1,36 +1,46 @@
+<?php
+
+$userId = objParamExistsOrDefault($user, 'id');
+$userImg = objParamExistsOrDefault($user, 'img');
+$userBio = objParamExistsOrDefault($user, 'bio');
+
+$userImgUrl = $BASE . '/' . imgOrDefault('users', $userImg, $userId);
+
+$nameUserProfile = objParamExistsOrDefault($user, 'name');
+$lastNameUserProfile = objParamExistsOrDefault($user, 'last_name');
+
+$emailUserProfile = objParamExistsOrDefault($user, 'email');
+
+?>
+
 <header class="imgBackgroundArea usersAdmBackground d-flex flex-wrap justify-content-center align-items-center flex-column">
     <span>
         <h2>Funcionário</h2>
-        <h1><?= $user->name ?> <?= $user->last_name ?></h1>
+        <h1><?= $nameUserProfile ?> <?= $lastNameUserProfile ?></h1>
     </span>
 </header>
 
 <section class="user d-flex flex-wrap align-items-center justify-items-center m-auto">
     <figure class="d-flex flex-wrap m-auto justify-content-center">
-        <img class="img-prevent-drag " src="<?= $BASE ?>/<?= imgOrDefault('users', $user->img, $user->id) ?>" title="<?= $user->name ?>" onerror="this.onerror=null;this.src='<?= $BASE ?>/public/resources/img/not_found/no_image.jpg';">
+        <img class="img-prevent-drag " src="<?= $userImgUrl ?>" title="<?= $nameUserProfile ?>" onerror="this.onerror=null;this.src='<?= $BASE ?>/public/resources/img/not_found/no_image.jpg';">
+
         <figcaption class="bg-light mt-5">
-            <h5>Sobre <?= $user->name ?></h5>
-            <p><?= $user->bio ?></p>
+            <h5>Sobre <?= $nameUserProfile ?></h5>
+            <p><?= $userBio ?></p>
         </figcaption>
     </figure>
 </section>
 
-<?= ($_SESSION['user_status'] != null) ? "<ul class='text-center'><li><a href='$BASE/users/edit/$user->id'>Id de usuário $user->id | Nome: $user->name | E-mail: $user->email</a></li></ul>" : ''; ?>
-
-<?php if (count($posts) > 0) :
-
-    // $linkCommonHtmlAttributes
-    //     = 'hx-push-url="true"  
-    // hx-swap="show:body:top"  
-    // hx-target="[data-js=\'result-itens-container\']"  
-    // hx-select="[data-js=\'result-itens-container\'] [data-js=\'loop-item\']"';
+<?php if ($_SESSION['user_status'] != null) echo "<ul class='text-center' hx-boost='true' hx-target='body' hx-swap='outerHTML'><li><a href='$BASE/users/edit/$userId'>Id de usuário $userId | Nome: $nameUserProfile | E-mail: $emailUserProfile</a></li></ul>";
 
 ?>
+
+<?php if (count($posts) > 0) : ?>
 
     <article class="userPosts flex flex-column justify-content-center" data-js="top-page-header">
 
         <header class="text-center m-auto p-3">
-            <h3>Postagens feitas por <?= $user->name ?></h3>
+            <h3>Postagens feitas por <?= $nameUserProfile ?></h3>
         </header>
 
         <?php
