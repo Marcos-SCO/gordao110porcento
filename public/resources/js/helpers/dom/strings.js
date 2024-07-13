290a4f4edb77a302b5f61589893a70ca10dbf93c
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-07-13 03:22:01 -0300

    fix: slug accent format

--- public/resources/js/helpers/dom/strings.js
+++ public/resources/js/helpers/dom/strings.js
@@ -1,4 +1,29 @@
+function getAccentMap() {
+  return {
+    'À': 'A', 'Á': 'A', 'Â': 'A', 'Ã': 'A', 'Ä': 'A', 'Å': 'A',
+    'à': 'a', 'á': 'a', 'â': 'a', 'ã': 'a', 'ä': 'a', 'å': 'a',
+    'È': 'E', 'É': 'E', 'Ê': 'E', 'Ë': 'E',
+    'è': 'e', 'é': 'e', 'ê': 'e', 'ë': 'e',
+    'Ì': 'I', 'Í': 'I', 'Î': 'I', 'Ï': 'I',
+    'ì': 'i', 'í': 'i', 'î': 'i', 'ï': 'i',
+    'Ò': 'O', 'Ó': 'O', 'Ô': 'O', 'Õ': 'O', 'Ö': 'O',
+    'ò': 'o', 'ó': 'o', 'ô': 'o', 'õ': 'o', 'ö': 'o',
+    'Ù': 'U', 'Ú': 'U', 'Û': 'U', 'Ü': 'U',
+    'ù': 'u', 'ú': 'u', 'û': 'u', 'ü': 'u',
+    'Ç': 'C', 'ç': 'c',
+    'Ñ': 'N', 'ñ': 'n',
+    'Ý': 'Y', 'ý': 'y', 'ÿ': 'y',
+    'Æ': 'AE', 'æ': 'ae', 'Ø': 'O', 'ø': 'o', 'Å': 'A', 'å': 'a'
+  };
+}
+
 function stringSlugFormat(slug) {
+  // Map accented characters to ASCII equivalents
+  const accentMap = getAccentMap();
+
+  // Replace accented characters
+  slug = slug.split('').map(char => accentMap[char] || char).join('');
+
   // Convert the string to lowercase
   let slugToLower = slug.toLowerCase();
 
